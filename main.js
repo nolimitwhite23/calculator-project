34ef95222376758b67b59d6b19d6a84907828e83
Dewight <120032970+nolimitwhite23@users.noreply.github.com>
2023-12-24 03:06:31 -0500

    Added the Calculator box + Functionality

---
+++ main.js
@@ -0,0 +1,7 @@
+const buttonOne = document.getElementById("#1")
+const resultBox = document.querySelector(".result-bar")
+let value = 12
+
+function updateText() {
+    resultBox.textContent = value
+}
